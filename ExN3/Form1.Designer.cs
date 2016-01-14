5b7d27e8e9de85504318405bab615e5306f51b02
TimurMingaziev <tminraz@gmail.com>
2016-01-14 15:10:10 +0200

    exn3

---
+++ ExN3/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace ExN3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loadPageButton = new System.Windows.Forms.Button();
+            this.pagePathText = new System.Windows.Forms.TextBox();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.searchParamText = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.loadFromXmlButton = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusText = new System.Windows.Forms.ToolStripStatusLabel();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // loadPageButton
+            // 
+            this.loadPageButton.Location = new System.Drawing.Point(220, 337);
+            this.loadPageButton.Name = "loadPageButton";
+            this.loadPageButton.Size = new System.Drawing.Size(75, 23);
+            this.loadPageButton.TabIndex = 1;
+            this.loadPageButton.Text = "Старт";
+            this.loadPageButton.UseVisualStyleBackColor = true;
+            this.loadPageButton.Click += new System.EventHandler(this.loadPageButton_Click);
+            // 
+            // pagePathText
+            // 
+            this.pagePathText.Location = new System.Drawing.Point(47, 339);
+            this.pagePathText.Name = "pagePathText";
+            this.pagePathText.Size = new System.Drawing.Size(168, 20);
+            this.pagePathText.TabIndex = 2;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(12, 12);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(450, 290);
+            this.treeView1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 342);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "URI:";
+            // 
+            // searchParamText
+            // 
+            this.searchParamText.Location = new System.Drawing.Point(105, 310);
+            this.searchParamText.MaxLength = 2;
+            this.searchParamText.Name = "searchParamText";
+            this.searchParamText.Size = new System.Drawing.Size(87, 20);
+            this.searchParamText.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 313);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Глубина поиска";
+            // 
+            // loadFromXmlButton
+            // 
+            this.loadFromXmlButton.Location = new System.Drawing.Point(344, 336);
+            this.loadFromXmlButton.Name = "loadFromXmlButton";
+            this.loadFromXmlButton.Size = new System.Drawing.Size(118, 23);
+            this.loadFromXmlButton.TabIndex = 7;
+            this.loadFromXmlButton.Text = "Загрузить из xml";
+            this.loadFromXmlButton.UseVisualStyleBackColor = true;
+            this.loadFromXmlButton.Click += new System.EventHandler(this.loadFromXmlButton_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel,
+            this.toolStripStatusLabel1,
+            this.statusText});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 404);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(475, 22);
+            this.statusStrip1.TabIndex = 9;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel
+            // 
+            this.toolStripStatusLabel.Name = "toolStripStatusLabel";
+            this.toolStripStatusLabel.Size = new System.Drawing.Size(0, 17);
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(62, 17);
+            this.toolStripStatusLabel1.Text = "Status bar:";
+            // 
+            // statusText
+            // 
+            this.statusText.Name = "statusText";
+            this.statusText.Size = new System.Drawing.Size(0, 17);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(15, 376);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(447, 16);
+            this.progressBar.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(475, 426);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.loadFromXmlButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.searchParamText);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.pagePathText);
+            this.Controls.Add(this.loadPageButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Ссылочки";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button loadPageButton;
+        private System.Windows.Forms.TextBox pagePathText;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox searchParamText;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button loadFromXmlButton;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel statusText;
+        private System.Windows.Forms.ProgressBar progressBar;
+    }
+}
+
---
+++ ExN3/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Xml.Serialization;
+
+namespace ExN3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string title = "";
+        List<string> titleList = new List<string>(); // Список для хранения шапок стр.
+        List<string> links = new List<string>(); // Списаок для хранения ссылок.
+
+        // Поиск тега <title> и запись содержимого тега в переменную title.
+        void getTitle(string inputString)
+        {
+            try
+            {
+                Match m = Regex.Match(inputString, @"<title>\s*(.+?)\s*</title>", RegexOptions.IgnoreCase | RegexOptions.Compiled);
+                if (m.Success)
+                {
+                    title = m.Groups[1].Value;
+                }
+            }
+            catch (RegexMatchTimeoutException)
+            {
+                statusText.Text = "The matching operation timed out.";
+            }
+        }
+
+        // Определяет URL в строке
+        string defineUrl(string input)
+        {
+            
+            string url = "";
+            Match m;
+            try
+            {
+                m = Regex.Match(input, @"(ht|f)tp(s?)\:\/\/[0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*(:(0-9)*)*(\/?)([a-zA-Z0-9\-\.\?\,\'\/\\\+&amp;%\$#_]*)?$", RegexOptions.IgnoreCase | RegexOptions.Compiled, TimeSpan.FromSeconds(1));
+                if (m.Success)
+                    try
+                    {
+                        Uri uri = new Uri(m.Groups[1].Value.ToString());
+                        if (!uri.IsFile && Uri.IsWellFormedUriString(m.Groups[1].Value.ToString(), UriKind.RelativeOrAbsolute))
+                            url = m.Groups[1].Value.ToString();
+                    }
+                    catch { }
+            }
+            catch (RegexMatchTimeoutException) { statusText.Text = "The matching operation timed out."; }
+            return url;
+        }
+
+        // Поиск ссылок в данной странице.
+        void searchHrefs(string inputString)
+        {
+            getTitle(inputString);
+            links.Clear();
+            Match m;
+            try
+            {
+                m = Regex.Match(inputString, "href\\s*=\\s*(?:[\"'](?<1>[^\"']*)[\"']|(?<1>\\S+))", RegexOptions.IgnoreCase | RegexOptions.Compiled, TimeSpan.FromSeconds(1));
+                while (m.Success)
+                {
+                    try
+                    {
+
+                        Uri uri = new Uri(m.Groups[1].Value.ToString());
+                        if (!uri.IsFile && Uri.IsWellFormedUriString(m.Groups[1].Value.ToString(), UriKind.RelativeOrAbsolute))
+                            links.Add(m.Groups[1].Value.ToString());
+                    }
+                    catch { }
+                    finally { m = m.NextMatch(); }
+                }
+            }
+            catch (RegexMatchTimeoutException)
+            {
+               statusText.Text = "The matching operation timed out.";
+            }
+        }
+
+        // Загрузка кода html страницы. Переменная path предназначена для выбора функции
+        void loadPage(string url, bool path)
+        {
+            
+            HttpWebResponse resp = null;
+            StreamReader reader = null;
+            string htmlCode = "";
+            try
+            {
+                Uri uri = new Uri(url);
+                try
+                {
+                    HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+
+                    resp = (HttpWebResponse)req.GetResponse();
+                    reader = new StreamReader(resp.GetResponseStream());
+                    htmlCode += reader.ReadToEnd();
+
+                }
+                catch (WebException ex) { statusText.Text = ex.Message + " " + url; }
+                catch (ProtocolViolationException ex) { statusText.Text = ex.Message + " " + url; }
+                catch (UriFormatException ex) { statusText.Text = ex.Message + " " + url; }
+                catch (IOException ex) { MessageBox.Show(ex.Message, "Ошибка ввода-вывода"); }
+                catch (NotSupportedException ex) { statusText.Text = ex.Message + " " + url; ; }
+                catch (System.Security.SecurityException ex) { statusText.Text = ex.Message + " " + url; }
+
+                finally
+                {
+                    if (resp != null)
+                        resp.Close();
+                    if (reader != null)
+                        reader.Close();
+                }
+                if (path)
+                    searchHrefs(htmlCode);
+                else getTitle(htmlCode);
+                
+            }
+            catch (Exception e) { statusText.Text = e.Message; }
+        }
+        private void loadPageButton_Click(object sender, EventArgs e)
+        {
+            treeView1.Nodes.Clear();
+            links.Clear();
+            try
+            {
+                Uri uri = new Uri(pagePathText.Text);
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); return; }
+            try
+            {
+                Convert.ToInt32(searchParamText.Text);
+            }
+            catch { MessageBox.Show("Не верный формат глубины"); return; }
+            TreeNode node = itsTree(pagePathText.Text, Convert.ToInt32(searchParamText.Text), new TreeNode());
+            foreach (TreeNode n in node.Nodes)
+            {
+                treeView1.Nodes.Add(n);
+            }
+            progressBar.Value = 0;
+            ClassSerialiaze.SaveTree(treeView1, "linksTree.xml");
+        }
+        
+        public TreeNode itsTree(string url, int l, TreeNode node){
+            loadPage(url,true);
+            progressBar.Value = 0;
+            progressBar.Minimum = 0;
+            progressBar.Maximum = links.Count;
+            progressBar.Step = 1;
+
+            IEnumerable<string> distinctAges = links.Distinct();
+            
+            foreach (string a in distinctAges)
+            {
+                progressBar.PerformStep();
+                loadPage(a, false);
+                node.Nodes.Add(title + " " + a);
+            }
+            progressBar.Value = links.Count;
+            if (l > 0)
+            {
+                try
+                {
+                    foreach (TreeNode n in node.Nodes)
+                    {
+                        itsTree(defineUrl(n.Text), l - 1, n);
+                    }
+                }
+                catch (ArgumentException e) { statusText.Text = e.Message; }
+
+            }
+            
+            return node;
+        }
+        private void loadFromXmlButton_Click(object sender, EventArgs e)
+        {
+            treeView1.Nodes.Clear();
+            ClassSerialiaze.LoadTree(treeView1, "linksTree.xml");
+        }
+    }
+}
